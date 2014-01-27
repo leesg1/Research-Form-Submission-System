2a669aa5ac97c7491382eb5440a231a1a7d2e834
Ciaran Egan <eganc6@tcd.ie>
2014-01-27 16:18:31 +0000

    Initial rails file structure initialised

---
+++ SubmissionReview/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SubmissionReview::Application.config.secret_token = '52ffb59ecaf3f805f0c540fa489db18134ef61bcc7b5b1b9aa7a0e61bc306cf1180522533e76c10f3af150caa81a78055f51afc57bef57f8b0634efbc4bc236f'
